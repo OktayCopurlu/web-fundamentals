17335089d50f141dddb37b4887f3cf4404037ef8
OktayCopurlu <selimhalim12@gmail.com>
2021-01-30 21:43:12 +0100

    todo

--- MODUL2-JS/todo-ogrenci/controller/function.js
+++ MODUL2-JS/todo-ogrenci/controller/function.js
@@ -13,30 +13,6 @@ function pushStudentToArray() {
     return eklenenOGrenciler.push(createStudent());
 }
 
-//öğrencileri arraydan alip tablo içine aktarma....
-function addStudent() {
-    return eklenenOGrenciler.map((i, index) => {
-        liste.innerHTML += `
-        <tr id="${index}">
-        <td>${i.isim}</td>
-        <td>${i.soyad}</td>
-        <td>${i.yas}</td>
-        <td><button class="btn btn-danger" id="silme-button">Sil</button></td>
-        </tr>`
-    }).join("");
-}
-
-//inputları doldurup öğrenciyi öğrenciyi ekrana yazdırma...
-eklemeButton.addEventListener("click", () => {
-    if (inputIsim.value != "" && inputSoyad.value != "" && inputYas.value != "") {
-        pushStudentToArray();
-        remoreInput();
-        addStudent();
-        refresh()
-    } else {
-        alert("Bilgileri Doğru Giriniz...")
-    }
-});
 
 //öğrenci ekleme butonuna bastiktan sonra input içini boşaltma...
 function remoreInput() {
